4491cc94daf240550907524a0a32be5afd57a270
Thanga Ayyanar <gldayan8@gmail.com>
2017-03-30 07:15:28 +0530

    trial two with answer grabing

--- Grab_Question.js
+++ Grab_Question.js
@@ -1,28 +1,46 @@
-// ==UserScript==
-// @name        aaa
 // @namespace   thangaayyanar.blogspot.com
 // @include     about:newtab
 // @version     1
 // @grant       none
 // ==/UserScript==
-//@require https://raw.githubusercontent.com/eligrey/FileSaver.js/master/FileSaver.js
-
-function magic(){
-  alert("hello");
+var soln="hellog";
+function hello(count){
+  solution(document.getElementById("solutionpanel").innerHTML,false,++count);
+}
+function solution(datam,flag,count){
   
-  /* this code is under construction
-  if (unsafeWindow.count == undefined) {
-   unsafeWindow.count = 0;
-  }
+  if(flag){
+      var str="qlist_tbl:"+count+":j_id_du";
+      document.getElementById(str).click();
+      setTimeout(function(){
+        if(count<5)
+         hello(count);  
+      },1000)
+    }else{  
+      soln+=datam;
+      //alert(soln);
+      if(count==5){
+         alert("in");
+          magic(false,soln);
+       }
+      solution(soln,true,count);
+    }
+     //console.log(soln);
+     //soln="helloworld";
+   
+    //unsafeWindow.count+=5;
+}
+function magic(flag,datam){
+  //alert("hello");
+  //if (unsafeWindow.count == undefined) {
+   //unsafeWindow.count = 0;
+  //}
+  if(flag){
+    solution("",true,0);
+  }else{
   var data=document.getElementById("qlist_tbl_data").innerHTML;
-  for(i=unsafeWindow.count;i<unsafeWindow.count+5;i++){
-    var str="qlist_tbl:"+i+":j_id_du";
-    data+=document.getElementById(str).click();
-    data+=document.getElementById("solutionpanel").innerHTML;
-    delay(200);
-  }
-  unsafeWindow.count+=5;
-  */
+  data+=datam;
+  //alert("log"+soln);
   var a = document.createElement('a');
   //a.href = 'data:application/csv;charset=utf-8,' + encodeURIComponent(data);
   a.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(data);
@@ -32,12 +50,13 @@ function magic(){
   document.getElementsByTagName('body')[0].appendChild(a);
   //supported by chrome 20+ and firefox 5+
   a.click();
+  }
 }
 (function(){
 document.addEventListener('keydown', function(e) {
   // pressed alt+g
   if (e.keyCode == 71 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-   magic();
+   magic(true,"");
   }
 }, false);
 })();
